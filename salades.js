ab8c7dd9599781f3d6febfa447bd7e6305aadf74
unknown <siwarhamdi861@gmail.coml>
2024-03-21 09:23:02 +0100

    siwarproj

---
+++ salades.js
@@ -0,0 +1,11 @@
+$("#btn1").click(function () {
+    $("#p1").css({"display":"block"})
+})
+$("#btn2").click(function () {
+    $("#p2").css({"display":"block"})
+})
+$("#btn3").click(function () {
+    $("#p3").css({"display":"block"})
+})
+
+
